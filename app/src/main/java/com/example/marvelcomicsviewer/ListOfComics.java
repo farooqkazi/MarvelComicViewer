73fdf37ee929f49547daa305bed47781e4dc7e7e
Sidd <sidd@Sidds-MacBook-Air.local>
2017-06-02 20:15:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/marvelcomicsviewer/ListOfComics.java
@@ -0,0 +1,13 @@
+package com.example.marvelcomicsviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListOfComics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_comics);
+    }
+}
